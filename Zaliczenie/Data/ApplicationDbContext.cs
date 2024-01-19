7046101b8dedb8450f5270d03c9ee1f7d7b8007d
Piotr Szczeszek <piotr.szczeszek00@gmail.com>
2024-01-19 18:26:48 +0100

    Init

---
+++ Zaliczenie/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Zaliczenie.Data.Entities;
+
+namespace Zaliczenie.Data;
+
+public class ApplicationDbContext : IdentityDbContext<User>
+{
+    public virtual DbSet<User> Users { get; set; } = null!;
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ Zaliczenie/Data/Entities/User.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Zaliczenie.Data.Entities;
+
+public class User : IdentityUser
+{
+    
+}

dcea28523a8cb7d7629a96356c6c498a5816f09c
Piotr Szczeszek <piotr.szczeszek00@gmail.com>
2024-01-20 17:13:05 +0100

    migracje w release

--- Zaliczenie/Program.cs
+++ Zaliczenie/Program.cs
@@ -41,6 +41,10 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
+    using var scope = app.Services.CreateScope();
+    var dbCtx = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbCtx.Database.Migrate();
+    
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
